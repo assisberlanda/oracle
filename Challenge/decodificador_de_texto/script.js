bbacf0de3407188fcdfff0de5fadb7cd735f8eb3
Assis Berlanda de Medeiros <99512049+assisberlanda@users.noreply.github.com>
2022-04-14 17:07:35 -0300

    Initial commit

---
+++ Challenge/decodificador_de_texto/script.js
@@ -0,0 +1,8 @@
+let codigo = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat'};
+
+
+let frase = document.querySelector('textarea');
+for (let i = 0; i < frase.length; i++);
+    codigo.replaceChildren(frase[i])
+
+    console.log(frase);
---
+++ aulajs.js
@@ -0,0 +1,6 @@
+function pulaLinha (n) {
+    document.write(n * "<br>")
+ };
+
+ pulaLinha(3);
+ 
