031b7c67aeffc66fa832de4da6e38122bbb71b17
Assis Berlanda de Medeiros <99512049+assisberlanda@users.noreply.github.com>
2022-04-18 16:55:57 -0300

    update alura

--- Challenge/decodificador_de_texto/script.js
+++ Challenge/decodificador_de_texto/script.js
@@ -1,8 +1,7 @@
 let codigo = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat'};
 
-
-let frase = document.querySelector('textarea');
-for (let i = 0; i < frase.length; i++);
-    codigo.replaceChildren(frase[i])
-
-    console.log(frase);
+let frase = 'gostaria que voce codificasse essa frase';//document.querySelector('textarea');
+for (let i = 0; i < codigo.length; i++) {
+    let fraseCodificada = frase.replace(/frase/g, codigo[i]);
+    console.log(fraseCodificada);
+}
